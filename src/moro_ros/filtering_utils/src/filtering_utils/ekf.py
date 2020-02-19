fix the jacobian part

src/moro_ros/filtering_utils/src/filtering_utils/ekf.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import numpy as np
 
-
+R
 class EKF:
     def __init__(self, state_vector_size, control_size, measurement_size):
         self.state_vector = np.zeros((state_vector_size, 1))
+        self.next_state = np.zeros((state_vector_size, 1))
         self.cov_matrix = 1000. * np.identity(state_vector_size)  #p
         self.q = np.zeros((control_size, control_size))
         #self.r = np.zeros((measurement_size,, measurement_size))
         #self.q = np.zeros((control_size, 1))
-        self.R = np.zeros((measurement_size, measurement_size))
+        self. = np.zeros((measurement_size, measurement_size))
         self.motion_j_state = np.zeros((state_vector_size, state_vector_size))
         self.motion_j_noise = np.zeros((state_vector_size, control_size))
         self.obs_j_state = np.zeros((measurement_size, state_vector_size))
@@ -18,27 +19,26 @@ class EKF:
         self.delta_w = 0
         self.t = 0
     def predict(self, x, y, theta, v, w):
-        
-        self.propagate_state(x, y, theta, v, w)
+        self.motion_jacobian_state_vector(x, y, theta, v, w)
+        self.motion_jacobian_noise_components(x, y, theta, v, w)
+        # the next state
+        self.next_state = self.propagate_state(x, y, theta, v, w)
         self.calculate_cov()
 
-    def update(self):
+    def update(self, x, y, theta):
+        self.observation_jacobian_state_vector(x, y, theta)
         K = (self.calculate_cov*self.obs_j_state.transpose())/(self.obs_j_state*self.calculate_cov*self.obs_j_state.transpose()+self.R)
         m_x, m_y, theta = self.state_vector[0], self.state_vector[1], self.state_vector[2]
-        self.state_vector = K*()
+        self.state_vector = self.next_state+K*()
         self.Q = (np.eye(self.obs_j_state.shape[0])-K*self.obs_j_state)
 
         pass
-
+    def h(self, x, y, l_x, l_y, theta):
+        Z = []
+        Z[0] = np.sqrt((x-l_x)**2+(y-l_y)**2)
+        Z[1] = np.arctan((y-l_y)/(x-l_x))-theta
     def propagate_state(self, x, y, theta, v, w):
         state_next = []
-        """
-        x = self.state_vector[0]
-        y = self.state_vector[1]
-        theta = self.state_vector[2]
-        v = self.q[0]
-        w  =self.q[1]
-        """
         state_next[0] = x + (v)*np.sin(theta+(w)*self.t)/(w)
         state_next[1] = y + (v)*np.cos(theta+(w)*self.t)/(w)
         state_next[2] = theta + (w)*self.t
@@ -50,32 +50,21 @@ class EKF:
         self.cov_matrix = self.motion_j_state * self.cov_matrix * \
             self.motion_j_state.transpose() + self.Q
 
-    def motion_jacobian_state_vector(self):
+    def motion_jacobian_state_vector(self, x, y, theta, v, w):
         # TO DO
-        x = self.state_vector[0]
-        y = self.state_vector[1]
-        theta = self.state_vector[2]
-        v = self.q[0]
-        w  =self.q[1]
-        self.motion_j_state = np.array([[1, 0, (v+self.delta_v)*np.cos(theta+(w+self.delta_w)*self.t)/(w+self.delta_w)],[0, 1, -(v+self.delta_v)*np.sin(theta+(w+self.delta_w)*self.t)/(w+self.delta_w)], [0, 0, 1]])
+        self.motion_j_state = np.array([[1, 0, (v+self.delta_v)*np.cos(theta+(w+self.delta_w)*self.t)/(w+self.delta_w)],[0, 1, (v+self.delta_v)*np.sin(theta+(w+self.delta_w)*self.t)/(w+self.delta_w)], [0, 0, 1]])
         # self.motion_j_state
         pass
 
-    def motion_jacobian_noise_components(self):
+    def motion_jacobian_noise_components(self, x, y, theta, v, w):
         # TO DO
-        x = self.state_vector[0]
-        y = self.state_vector[1]
-        theta = self.state_vector[2]
-        v = self.q[0]
-        w  =self.q[1]
+        """
         self.motion_j_noise = np.array([[np.sin(theta+(w+self.delta_w)*self.t)/(w+self.delta_w), -(v+self.delta_v)*np.cos(theta+(w+self.delta_w)*self.t)*self.t/(w+self.delta_w)**2], [np.cos(theta+(w+self.delta_w)*self.t)/(w+self.delta_w), (v+self.delta_v)*np.sin(theta+(w+self.delta_w)*self.t)*self.t/(w+self.delta_w)**2], [0, self.t]])
+        """
         pass
 
-    def observation_jacobian_state_vector(self):
+    def observation_jacobian_state_vector(self, x, y, theta):
         # To DO
-        x = self.state_vector[0]
-        y = self.state_vector[1]
-        #theta = self.state_vector[2]
         self.obs_j_state = np.array([[x/np.sqrt(x**2+y**2), y/np.sqrt(x**2+y**2), 0], [0, 0, x**2/(x**2+y**2)]])
         pass
     

src/moro_ros/moro_localization/src/ekf_localization.py
@@ -9,6 +9,7 @@ from marker_msgs.msg import MarkerDetection
 from filtering_utils.ekf import EKF
 
 ekf = EKF(3, 2, 2)
+# initialize 
 
 
 def odom_callback(msg):
@@ -21,10 +22,10 @@ def odom_callback(msg):
     v = np.sqrt(v_x**2+v_y**2)
     theta = np.arctan(v_y/v_x)
     ekf.t = 50
-    ekf.q=np.diag([v, w])
+    ekf.q=np.diag()
     ekf.state_vector=np.array([v_x*ekf.t, v_y*ekf.t, theta])
     # pass 
-    ekf.predict()
+    ekf.predict(ekf.state_vector[0], ekf.state_vector[1], ekf.state_vector[2], v, theta)
     
     #predict_co_matrix
     pass